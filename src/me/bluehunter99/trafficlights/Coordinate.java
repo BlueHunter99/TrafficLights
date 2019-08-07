0b877895baf912ba2403edffeb41964c73c87a44
BlueHunter99 <joeri.meijer2013@gmail.com>
2019-08-07 11:30:31 +0200

    Language change to English (mostly)
    Signed-off-by: BlueHunter99 <joeri.meijer2013@gmail.com>

--- src/me/bluehunter99/trafficlights/Coordinate.java
+++ src/me/bluehunter99/trafficlights/Coordinate.java
@@ -1,24 +1,22 @@
 package me.bluehunter99.trafficlights;
 
 /**
- * De Coordinate klasse is een een klasse om een simpel coordinaat te hebben.
+ * The Coordinate class represents a simple coordinate.
  *
- * @author Joeri
+ * @author BlueHunter99
  *
  */
 
 public class Coordinate {
 
-    // Variabelen
+    // Variables
     public int x, y;
 
     /**
      * Constructor
      *
-     * @param x:
-     *            x positie
-     * @param y:
-     *            y positie
+     * @param x: x position
+     * @param y: y position
      */
     public Coordinate(int x, int y) {
         this.x = x;
